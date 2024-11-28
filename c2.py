054d63073cdd0cec956cca24c05977e812a15d3c
kagacewho <whykpsad@tutamail.com>
2024-11-28 23:06:48 +0300

    Update c2.py

--- c2.py
+++ c2.py
@@ -1,14 +1,38 @@
-print('Калькулятор')
+def mult(a, b):
+    return a * b
 
-def summa(a,b):
-    return a+b
+def summa(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b 
 
-a = int(input('Укажите первое число: '))  
+def div(a, b):
+    if b == 0:
+        return "Ошибка: деление на ноль"
+    return a / b
 
+def separate(string: str, separator):
+    result = string.split(separator)
+    return [int(x) for x in result]  # Преобразование в список чисел
 
-b = int(input('Укажите второе число: ')) 
+user_input = input('Введите выражение: ')
 
-summ = a + b
+if "+" in user_input:
+    numbers = separate(user_input, "+")
+    print(summa(numbers[0], numbers[1]))
 
-print(f'Сумма чисел: {summ}')
+elif "-" in user_input:
+    numbers = separate(user_input, "-")
+    print(sub(numbers[0], numbers[1]))
+
+elif "*" in user_input:
+    numbers = separate(user_input, "*")
+    print(mult(numbers[0], numbers[1]))
+
+elif "/" in user_input:
+    numbers = separate(user_input, "/")
+    print(div(numbers[0], numbers[1]))
+
+else:
+    print('Неправильный ввод')
